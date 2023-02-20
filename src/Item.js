666b6402dfde1a6958997429655ec41f49aa4e2f
Apurv2307 <apurvtewari.nuks@gmail.com>
2023-02-20 15:09:07 +0530

    new commit

--- src/Item.js
+++ src/Item.js
@@ -4,10 +4,11 @@ export default styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 250px;
-  width: 100%;
+  height: 100px;
+  width: 250px;
   background-color: #00008B;
   color: #fff;
-  margin: 0 15px;
-  font-size: 4em;
+  margin: 0 5px;
+  font-size: 3em;
+  border-radius: 8%;
 `;
--- src/index.js
+++ src/index.js
@@ -6,15 +6,17 @@ import "./styles.css";
 
 const breakPoints = [
   { width: 1, itemsToShow: 1 },
-  { width: 550, itemsToShow: 2 },
-  { width: 768, itemsToShow: 3 },
-  { width: 1200, itemsToShow: 4 },
+  { width: 450, itemsToShow: 2 },
+  { width: 668, itemsToShow: 3 },
+  { width: 1000, itemsToShow: 4 },
+  { width: 1200, itemsToShow: 5 },
+  { width: 1500, itemsToShow: 6 },
 ];
 
 function App() {
   return (
     <>
-      <h1 style={{ textAlign: "center" }}>Example to setup your carousel in react</h1>
+      <h1 style={{ textAlign: "center" }}>SELECT FROM CATEGORIES</h1>
       <div className="App">
         <Carousel breakPoints={breakPoints}>
           <Item>One</Item>
